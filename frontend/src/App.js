251f6eec39c3ee7458fcbfad99822ed57e1991a6
Tyler <tyler.jankowski@hotmail.com>
2023-01-01 23:19:43 -0500

    added home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
+import HomeScreen from './screens/HomeScreen'
+
 
 function App() {
   return (
@@ -12,9 +14,7 @@ function App() {
       
       <main className="py-3">
           <Container>
-            <h1>
-              Welcome
-            </h1>
+            <HomeScreen />
           </Container>
       </main>
 
