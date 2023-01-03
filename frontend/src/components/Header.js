8c94d08f97888aee1490ee82ae394f9727e20d59
Tyler <tyler.jankowski@hotmail.com>
2023-01-03 13:34:14 -0500

    add routes and product pages

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Navbar, Nav, Container, Row } from 'react-bootstrap'
+import { Navbar, Nav, Container} from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 
 function Header() {
@@ -7,12 +8,22 @@ function Header() {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container fluid>
-          <Navbar.Brand href="/">JankyShop</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>JankyShop</Navbar.Brand>
+          </LinkContainer>
+          
+          
           <Navbar.Toggle aria-controls="mynav" />
           <Navbar.Collapse id="mynav">
             <Nav className="mr-auto">
-              <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
-              <Nav.Link href="/login"><i className='fas fa-user'></i>Login</Nav.Link>
+              
+              <LinkContainer to='/cart'>
+                <Nav.Link><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+              </LinkContainer>
+              
+              <LinkContainer to='/login'>
+                <Nav.Link><i className='fas fa-user'></i>Login</Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
 
-import Product from './components/Product'
+import Product from '../components/Product'
+
 
 import products from '../products'
 
