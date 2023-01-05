7411362c8bd61a7cc2d627d9462114ef9c2a885e
Tyler <tyler.jankowski@hotmail.com>
2023-01-05 12:27:58 -0500

    backend and frontend are talking to eachother

---
+++ backend/base/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', view=views.getRoutes, name="routes"),
+    path('products/', view=views.getProducts, name="products"),
+    path('products/<str:pk>', view=views.getProduct, name="product"),
+]
+
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import axios from 'axios'
 
 import Rating from '../components/Rating'
-import products from '../products'
 
 
 function ProductScreen() {
     const { id } = useParams();
-    const product = products.find((p) => String(p._id) === id);
+    const [product, setProduct] = useState([])
+
+    useEffect(() => {
+
+    async function fetchProduct() {
+        const { data } = await axios.get('/api/products/' + id)
+        setProduct(data)
+    }
+
+    fetchProduct()
+
+  }, [])
     return (
         <div>
             <Link to='/' className='btn btn-light my-3'>Go Back</Link>
@@ -65,7 +76,7 @@ function ProductScreen() {
                             </ListGroup.Item>
 
                             <ListGroup.Item>
-                                <Button className='btn-block' disabled={product.countInStock == 0} type='button'>Add To Cart</Button>
+                                <Button className='btn-block' disabled={product.countInStock === 0} type='button'>Add To Cart</Button>
                             </ListGroup.Item>
                         </ListGroup>
                     </Card>
