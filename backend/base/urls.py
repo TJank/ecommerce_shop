555739f6c647dce45457f28c5cfef2297514d675
Tyler <tyler.jankowski@hotmail.com>
2023-01-21 18:48:42 -0500

    added authentication and refactor views and urls

--- backend/base/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', view=views.getRoutes, name="routes"),
-    path('products/', view=views.getProducts, name="products"),
-    path('products/<str:pk>', view=views.getProduct, name="product"),
-]
-
